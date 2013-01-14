c6cb9d859c59e5028b2049f320b7cc67a43e3206
ned rihine <ned.rihine@gmail.com>
2013-01-14 12:40:53 +0900

    コンテキストメニューに「ぴんどる」アイテムを追加。

--- addon/data/pindrome-context-item.js
+++ addon/data/pindrome-context-item.js
@@ -1,3 +1,4 @@
 self.on( 'click', function (node, data) {
+    console.log( "self in click: " + data );
     self.port.emit( 'get-data', node.src );
 } );
--- addon/lib/main.js
+++ addon/lib/main.js
@@ -3,8 +3,15 @@ var self = require( 'self' );
 var ss = require( 'single-storage' );
 
 
-ss.storage.imageLists = [];
+/*
+ * シンプルストレージに imageList フィールドを追加します。
+ */
+ss.storage.imageList = [];
 
+
+/*
+ * 「ぴんどる」のコンテキストメニューを作成します。
+ */
 context_item = ContextMenu.Item( {
     label: "ぴんどる",
     context: ContextMenu.SelectorContext( 'img' ),
@@ -15,7 +22,10 @@ context_item = ContextMenu.Item( {
 } );
 
 
+/*
+ * get-data イベントのハンドラです。
+ */
 context_item.port.on( 'get-data', function (node_src) {
-    console.log( "node_src: " + node_src );
-    ss.storage.imageLists.push( node_src );
+    console.log( "in get-data: node_src: " + node_src );
+    ss.storage.imageList.push( node_src );
 } );
