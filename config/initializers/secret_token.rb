e24b5ddd877c6fc3200703b5409c52965b24f139
ned rihine <ned.rihine@gmail.com>
2013-03-10 01:38:54 +0900

    rails new Pindrome

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pindrome::Application.config.secret_token = 'dea913451b14fc0e1c7762bd24a0900ba89522b3907a19fb896c56f5d53d97f1d1a82bdca68992ab786eb7ec49437c2f77f823f32ee091263aa971f793d476fd'
